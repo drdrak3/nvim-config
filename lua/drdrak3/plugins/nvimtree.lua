d9fb9ecbf8c5dd1362f3b4072ef78c49eab12dd3
Matt Boik <matt@rosterfy.com>
2026-01-07 12:25:12 +1100

    Increase nvimtree width

--- lua/drdrak3/plugins/nvimtree.lua
+++ lua/drdrak3/plugins/nvimtree.lua
@@ -30,7 +30,7 @@ return {
       view = {
         adaptive_size = false,
         side = 'left',
-        width = 30,
+        width = 35,
         preserve_window_proportions = true,
       },
       git = {
